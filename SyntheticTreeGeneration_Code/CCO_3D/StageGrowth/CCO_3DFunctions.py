f26ff4675827b3d7238f8425279ecb155fe63e74
ClaraJaquet <clara.jaquet@esiee.fr>
2016-10-28 11:14:53 +0200

    adding 3d staged growth

---
+++ SyntheticTreeGeneration_Code/CCO_3D/StageGrowth/CCO_3DFunctions.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 16:18:32 2016
+
+@author: cjaquet
+"""
+
+import numpy as np
+import sys
+import copy
+from skimage.segmentation import random_walker
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pylab as pl
+
+#from pyamg import *
+
+###########################################Non convex functions####################
+## creating a potential grid using random walker
+#inner surface and concavity potential value is 1
+#outer surface potential value is 0
+def potential_image(center, ext_radius_f, int_radius_f, half):
+    #initialization
+    cx,cy,cz = int(center[0]), int(center[1]),int(center[2])
+    ext_radius = int(ext_radius_f)
+    int_radius =int(int_radius_f)
+    im = np.zeros((cx+ext_radius*3, cy+ext_radius*3,cz+ext_radius*3)).astype('uint8')
+    markers = np.zeros((cx+ext_radius*3, cy+ext_radius*3, cz+ext_radius*3)).astype('uint8')
+    margin = int(np.ceil(ext_radius/10.))
+    # grids of index   
+    z,y,x = np.ogrid[-ext_radius-margin: ext_radius+margin, -ext_radius-margin: ext_radius+margin, -ext_radius-margin:ext_radius+margin]  
+    z_i, y_i, x_i = np.ogrid[-int_radius: int_radius, -int_radius: int_radius,-int_radius: int_radius]
+    
+    #mask creation
+    index = x**2 + y**2 + z**2 <= ext_radius**2
+    im[cz-ext_radius-margin:cz+ext_radius+margin, cy-ext_radius-margin:cy+ext_radius+margin, cx-ext_radius-margin:cx+ext_radius+margin][index] = 1
+    index_int = x_i**2 + y_i**2 + z_i**2<= int_radius**2
+    im[cz-int_radius:cz+int_radius, cy-int_radius:cy+int_radius, cx-int_radius:cx+int_radius][index_int] = 0
+
+    #marker creation
+    #external
+    index = x**2 + y**2 +z**2 > ext_radius**2
+    markers[cz-ext_radius-margin:cz+ext_radius+margin, cy-ext_radius-margin:cy+ext_radius+margin, cx-ext_radius-margin:cx+ext_radius+margin][index] = 2
+    #internal
+    index_int = x_i**2 + y_i**2 +z_i**2 < int_radius**2
+    markers[cz-int_radius:cz+int_radius, cy-int_radius:cy+int_radius, cx-int_radius:cx+int_radius][index_int] = 3
+
+    #random_walker    
+    
+    result = random_walker(im, markers, copy =True, return_full_prob = True, mode= 'cg_mg')   
+    print "rdm walker shape",result.shape
+    print markers.shape
+    if half :
+        result[1][0:center[0],:,:] = 0
+    
+#    slicing = center[2]
+#    fig = plt.figure(figsize=(8, 8))
+#    ax = fig.add_subplot(111,projection='3d')#a3.Axes3D()#pl.figure(figsize=(wid, hei))
+#    #ax.imshow(im[20],20)
+#    xx, yy = pl.ogrid[0:im.shape[1], 0:im.shape[2]]
+#    #xx, yy = np.meshgrid(np.linspace(0,1,12), np.linspace(0,1,13))
+#    # create vertices for a rotated mesh (3D rotation matrix)
+#    X =  xx 
+#    Y =  yy
+#    Z =  slicing*np.ones(X.shape)
+#    #fig = plt.figure(projection='3d')
+#    
+#    
+#    #m = cm.ScalarMappable(cmap=cm.jet)
+#    #m.set_array(markers[:,:,slicing])
+#    
+#    #cmap for markers
+#    colors = [(1.0,1.0,1.0)]
+#    colors.extend([(0.0,0.0,1.)])
+#    colors.extend([(1.0,0.72,0.06)])
+#    colors.extend([(1.0,0.0,0.0)])
+#    cmap = mpl.colors.ListedColormap(colors)
+#    
+#    #cmap for random walker
+#    N=50
+#    colors = [(1.0,1.0,1.0)]
+#    colors.extend(plt.cm.jet(np.linspace(0., 1., N)))
+#    colors.extend([(1.0,1.0,1.0)])
+#    cmap =mpl.colors.ListedColormap(colors) #plt.cm.jet
+#    
+#    ax.plot_surface(Z,Y,X, rstride=1, cstride=1, facecolors=cmap(result[1][slicing,:,:]),shade=False)
+#    #ax.colorbar()#maping,ax=ax#maping, shrink=0.5, aspect=5
+#    ax.set_xlabel('X axis')
+#    ax.set_ylabel('Y axis')
+#    ax.set_zlabel('Z axis')
+#    plt.show()
+    return result[1]
+    
+    
+### Newton Raphson algorithm : 
+#looking for the location reaching the potential target_w along a line (defined by target line vector)
+#considering the local gradient at point
+#iterating until find a potential respecting epsilon tolerance
+
+def length(vect):
+    return np.sqrt(np.sum(vect**2))
+    
+#########################################################
+
+## distance criteria
+# n_term is the final number of terminal segment
+# k_term is the current number of terminal segment (at the current growth step)
+
+#v_perf is the total perfused volume     
+def calculate_d_tresh_3D(r_supp, k_term): 
+    r_pk = np.power((k_term + 1)* r_supp**3,3)
+    return np.power(np.pi*4.*np.power(r_pk,3)/(3.*k_term),1./3.),r_pk
+    
+## random location generation
+def random_location(center, radius):
+    max_border = np.max(center+radius)+3
+    #print "mqx border", max_border
+    #print "max border", max_border
+    position = np.random.rand(3)*max_border
+    #print "rdm position", position
+    return position
+       
+    
+
+
+#######################################################
+
+def test_connection_list(list_input):
+    #copy_tree = copy.deepcopy(list_input[0])
+    return list_input[0].test_connection(list_input[1], list_input[2],list_input[3], list_input[4])
+
+
+#########################################################
+
+#sibling ratio is r_child_0 / r_child_1
+def calculate_betas(sibling_ratio, gamma):
+    beta_child_0 = np.power(1 + sibling_ratio**-gamma, -1./gamma)
+    beta_child_1 = np.power(1 + sibling_ratio**gamma, -1./gamma)
+    #print "beta child_0", beta_child_0, "beta child_1", beta_child_1
+    return np.array([beta_child_0, beta_child_1])
+    
+#return distance between point and segment
+def segment_distance(seg_pt_a, seg_pt_b, point):
+    vect_ab = seg_pt_b - seg_pt_a
+    vect_ap = point - seg_pt_a
+    vect_bp = point - seg_pt_b
+    squared_length_ab = float(np.sum(vect_ab**2))
+    if (squared_length_ab == 0.): 
+        return np.sqrt(np.sum(vect_ap**2))
+    
+    relative_position = (np.sum(vect_ap*vect_ab)) / squared_length_ab
+    if (relative_position < 0.): # outside segment passed point a    
+        return  np.sqrt(np.sum(vect_ap**2))
+    elif (relative_position > 1.): #outside the segment passed point b     
+        return np.sqrt(np.sum(vect_bp**2))
+
+    projected_point = seg_pt_a + relative_position * vect_ab
+    vect_projp = point - projected_point
+    return np.sqrt(np.sum(vect_projp**2))
+  
+#return distance between line and point  
+def line_distance(seg_pt_a, seg_pt_b, point):
+    vec_ab = seg_pt_b - seg_pt_a
+    squared_length_ab = np.sum(vec_ab**2)
+    
+    #a and b on top of each other
+    if (squared_length_ab < sys.float_info.epsilon):
+        return -1.
+    
+    vec_apoint = point - seg_pt_a
+    vec_ab_dot_vec_apoint = np.sum(vec_apoint * vec_ab)
+    relative_position = vec_ab_dot_vec_apoint / squared_length_ab
+    
+    projected_point = seg_pt_a + relative_position * vec_ab
+    normal_vec = projected_point - point
+    return np.sqrt(np.sum(normal_vec**2))
+
+#test if any of the 3 segments has a length close to 0 (the bifurcation has degenerated into 2 segments)  
+def degenerating_test(c0, c1, c2, branching_location, radii, length_factor):
+    seg_parent_length = np.sqrt(np.sum((c0 - branching_location)**2)) * length_factor
+    seg_old_child_length = np.sqrt(np.sum((c1 - branching_location)**2)) * length_factor
+    seg_new_child_length = np.sqrt(np.sum((c2 - branching_location)**2)) * length_factor
+
+    if (seg_parent_length < 2.*radii[0]):
+        #print "parent seg degenerate"            
+        return False
+
+    if (seg_old_child_length < 2.*radii[1]):
+        #print "old child seg degenerate"
+        return False
+ 
+    if (seg_new_child_length < 2.*radii[2]):
+        #print "new child seg degenerate"
+        return False
+    
+    return True
+
+
+    
+def closestDistanceBetweenLines(a0,a1,b0,b1,clampAll=False,clampA0=False,clampA1=False,clampB0=False,clampB1=False):
+    ''' Given two line segments defined by numpy.array pairs (a0,a1,b0,b1)
+        Return the two closest points, and the distance
+    '''
+
+    # If clampAll=True, set all clamps to True
+    if clampAll:
+        clampA0=True
+        clampA1=True
+        clampB0=True
+        clampB1=True
+
+    # Calculate denomitator
+    A = a1 - a0
+    B = b1 - b0
+
+    _A = A / np.linalg.norm(A)
+    _B = B / np.linalg.norm(B)
+    cross = np.cross(_A, _B);
+
+    denom = np.linalg.norm(cross)**2
+
+
+    # If denominator is 0, lines are parallel: Calculate distance with a projection
+    # and evaluate clamp edge cases
+    if (denom == 0):
+        d0 = np.dot(_A,(b0-a0))
+        d = np.linalg.norm(((d0*_A)+a0)-b0)
+
+        # If clamping: the only time we'll get closest points will be when lines don't overlap at all.
+        # Find if segments overlap using dot products.
+        if clampA0 or clampA1 or clampB0 or clampB1:
+            d1 = np.dot(_A,(b1-a0))
+
+            # Is segment B before A?
+            if d0 <= 0 >= d1:
+                if clampA0 == True and clampB1 == True:
+                    if np.absolute(d0) < np.absolute(d1):
+                        return b0,a0,np.linalg.norm(b0-a0)
+                    return b1,a0,np.linalg.norm(b1-a0)
+
+            # Is segment B after A?
+            elif d0 >= np.linalg.norm(A) <= d1:
+                if clampA1 == True and clampB0 == True:
+                    if np.absolute(d0) < np.absolute(d1):
+                        return b0,a1,np.linalg.norm(b0-a1)
+                    return b1,a1,np.linalg.norm(b1,a1)
+
+        # If clamping is off, or segments overlapped, we have infinite results, just return position.
+        return None,None,d
+
+
+    # Lines criss-cross: Calculate the dereminent and return points
+    t = (b0 - a0);
+    det0 = np.linalg.det([t, _B, cross])
+    det1 = np.linalg.det([t, _A, cross])
+
+    t0 = det0/denom;
+    t1 = det1/denom;
+
+    pA = a0 + (_A * t0);
+    pB = b0 + (_B * t1);
+
+    # Clamp results to line segments if needed
+    if clampA0 or clampA1 or clampB0 or clampB1:
+
+        if t0 < 0 and clampA0:
+            pA = a0
+        elif t0 > np.linalg.norm(A) and clampA1:
+            pA = a1
+
+        if t1 < 0 and clampB0:
+            pB = b0
+        elif t1 > np.linalg.norm(B) and clampB1:
+            pB = b1
+
+    d = np.linalg.norm(pA-pB)
+
+    return pA,pB,d
+    
+#test if segment ab intersect with cd considering their respective width
+def no_overlap(point_a, point_b, point_c, point_d, width_ab, width_cd):
+    p1,p0,dist = closestDistanceBetweenLines(point_a, point_b,point_c,point_d, True)
+    if dist < (width_ab + width_cd):
+        return False
+    else :
+        return True
+        
+    
+def normalize(vector):
+    length = np.sqrt(np.sum(vector**2, axis = 0))
+    if length != 0:
+        return vector/length
+    else:
+        return vector
+        
+def dot(vec_a, vec_b):
+    return vec_a[0]*vec_b[0] + vec_a[1]*vec_b[1]    
+    
+    
+def determinant(pt_a, pt_b,pt_c,pt_d):
+    denom = (pt_a[0] - pt_b[0])*(pt_c[1] - pt_d[1]) - (pt_a[1] - pt_b[1])*(pt_c[0] - pt_d[0])
+    if (denom == 0.):
+        return False, np.empty(2)
+    x = ( (pt_a[0]*pt_b[1] - pt_a[1]*pt_b[0])*(pt_c[0]-pt_d[0]) - (pt_a[0] - pt_b[0])*(pt_c[0]*pt_d[1] - pt_c[1]*pt_d[0]) ) / denom 
+    y = ( (pt_a[0]*pt_b[1] - pt_a[1]*pt_b[0])*(pt_c[1]-pt_d[1]) - (pt_a[1] - pt_b[1])*(pt_c[0]*pt_d[1] - pt_c[1]*pt_d[0]) ) / denom
+    return True, np.array([x,y])
+    
+
+
---
+++ SyntheticTreeGeneration_Code/CCO_3D/StageGrowth/Kamiya3D.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 18 11:34:54 2016
+
+@author: cjaquet
+
+ Kamiya algo: single bifurcation optimization 
+based on minimal intravascular volume and physiological constraints
+
+
+"""
+
+
+import numpy as np
+from scipy import optimize
+
+### loop process
+# given a bifurcation starting_point (convexe average of the 3 points), and estimated radii (using tree existing segments) 
+# calculate segments radii with Poiseuille and minimal vascular volume (non linear system)
+# use them to calculate new bifurcation point, which serves as a new input to calculate again radii, and so on... 
+# if position converges and total tree volume decreases: solved 
+
+
+#ci contains coordinate xi and y1 of the point
+#f contains f0,f1,f2
+#returns position of starting point
+def starting_point(c0,c1,c2,f):
+    if (len(c0) != 2) or (len(c1) != 2) or (len(c2) != 2) or (len(f)!=3):
+        print "error in starting point inputs"
+        return 0.,0.
+    x_coord = (c0[0]*f[0] + c1[0]*f[1] + c2[0]*f[2]) / (2* f[0])
+    y_coord = (c0[1]*f[0] + c1[1]*f[1] + c2[1]*f[2]) / (2* f[0])
+    z_coord = (c0[2]*f[0] + c1[2]*f[1] + c2[2]*f[2]) / (2* f[0])
+    return np.array([x_coord, y_coord,z_coord])
+    
+def calculate_segment_lengths(c0,c1,c2,coords, length_factor):
+    l = np.ones(3)
+    l[0] = rescaled_length(c0 - coords, length_factor)
+    l[1] = rescaled_length(c1 - coords, length_factor)
+    l[2] = rescaled_length(c2 - coords, length_factor)
+    return l
+
+#dp are the pressure loss: P1-P0 and P2-P0 
+#the r_ori is an estimated one to serve the iteration start
+def calculate_dp_from_Poiseuille(f,l,r_ori, visc):
+    kappa = 8 * visc / np.pi    
+    r0_ori4 = r_ori[0]*r_ori[0]*r_ori[0]*r_ori[0]
+    r1_ori4 = r_ori[1]*r_ori[1]*r_ori[1]*r_ori[1]
+    r2_ori4 = r_ori[2]*r_ori[2]*r_ori[2]*r_ori[2]
+    dp1 = kappa * (f[0]*l[0]/r0_ori4 + f[1]*l[1]/r1_ori4)
+    dp2 = kappa * (f[0]*l[0]/r0_ori4 + f[2]*l[2]/r2_ori4)
+    return dp1,dp2 
+       
+#sqrd_r contains r0**2, r1**2, r2**2
+def calculate_new_bif_coords(c0,c1,c2,f,l,sqrd_r):
+    new_x = ( c0[0]*sqrd_r[0]/l[0] + c1[0]*sqrd_r[1]/l[1] + c2[0]*sqrd_r[2]/l[2] ) /  ( sqrd_r[0]/l[0] + sqrd_r[1]/l[1] + sqrd_r[2]/l[2] )
+    new_y = ( c0[1]*sqrd_r[0]/l[0] + c1[1]*sqrd_r[1]/l[1] + c2[1]*sqrd_r[2]/l[2] ) /  ( sqrd_r[0]/l[0] + sqrd_r[1]/l[1] + sqrd_r[2]/l[2] )
+    new_z = ( c0[2]*sqrd_r[0]/l[0] + c1[2]*sqrd_r[1]/l[1] + c2[2]*sqrd_r[2]/l[2] ) /  ( sqrd_r[0]/l[0] + sqrd_r[1]/l[1] + sqrd_r[2]/l[2] )      
+    return new_x, new_y,new_z
+    
+    
+def non_linear_solver(f,l,k,dp1,dp2,r_ori,gamma):
+    def func(z):
+        r1=z[0]
+        r2=z[1]
+        zeta= 3+gamma
+        r14= r1*r1
+        r16 = r1**(zeta/2.) #r14*r1
+        r24= r2*r2
+        r26 = r2**(zeta/2.)#r24*r2
+        ans = ( dp1/k*r14*(np.power(f[0]*(r16/f[1]+r26/f[2]),4./zeta )) - f[0]*l[0]*r14 - f[1]*l[1]*np.power(f[0]*(r16/f[1] +r26/f[2]), 4./zeta),
+                dp2/k*r24*(np.power(f[0]*(r16/f[1]+r26/f[2]),4./zeta )) - f[0]*l[0]*r24 - f[2]*l[2]*np.power(f[0]*(r16/f[1] +r26/f[2]), 4./zeta))         
+        return(ans)
+    #print"r_ori",  r_ori
+    dict_f={}
+    dict_f['solver']="root"
+    dict_f['method']="hybr"
+    dict_f['disp']= False
+    #optimize.optimize.
+    #optimize.show_options(solver=None, method = None, disp =  False)
+    sol=optimize.root(func,r_ori, jac = False )# options = {'solver' : 'root', 'method' :'hybr','disp':False}
+    #print "sol message", sol.message
+    #print "Value =", sol.fun
+    #print "sol", sol.x
+    
+    return sol.success, sol.x
+    
+#l contains l0, l1, l2
+def calculate_squared_radii(f, l, dp1, dp2, r_ori, visc, gamma):
+    k=8*visc/np.pi
+    success, r_1_2 = non_linear_solver(f,l,k,dp1,dp2, r_ori,gamma)
+    r1 = r_1_2[0]
+    r2 = r_1_2[1]
+    if (success == True):
+        r0 = np.power(f[0]*(r1**gamma/f[1] + r2**gamma/f[2]) , 1./gamma)  
+        return r0,r1,r2
+    else:
+        return 0.,0.,0.
+        
+def single_bif_volume(r, l):
+    return np.sum(np.pi*(r**2)*l, axis = 0)
+    
+#input r : r[0] and r[1] are the radii before connection added (r0 = r1 = r1 = radius of original segment before connection added) 
+#        this estimated r is used to calculate dp, then is updated in the calculate_radii function     
+def kamiya_loop_r2(coords_ini,c0,c1,c2,f, r, length_factor, visc,gamma):
+    l = calculate_segment_lengths(c0,c1,c2,coords_ini, length_factor)
+    dp1, dp2 = calculate_dp_from_Poiseuille(f,l,r, visc)
+    r[0], r[1], r[2] = calculate_squared_radii(f,l,dp1, dp2, np.power(r,2)[1:3], visc,gamma)
+    if (r[0] == 0.):
+        return False, coords_ini, np.sqrt(r), l
+    else:        
+        x_new,y_new,z_new = calculate_new_bif_coords(c0,c1,c2,f,l,r)
+        return True, np.array([x_new, y_new,z_new]), np.sqrt(r), l  
+        
+#when calculating segment length, need to consider a factor 
+#because current coordinates are scaled from a bigger perfusion territory
+def rescaled_length(vector, factor):
+    return np.sqrt(np.sum(vector**2))*factor
+
+def calculate_seg_lengths(c0,c1,c2,coords, length_factor):
+    l = np.zeros(3)
+    l[0] = rescaled_length(c0 - coords, length_factor)
+    l[1] = rescaled_length(c1 - coords, length_factor)
+    l[2] = rescaled_length(c2 - coords, length_factor)
+    return l
+
+
+# r =r0,r1,r2 in mm
+# f = flow in mm3/s
+# c are coordinates of each segment end
+def kamiya_single_bif_opt(r, f, c0, c1, c2, iter_max, tolerance, store_whole_results, length_factor):
+    xyz = starting_point(c0,c1,c2,f)
+    print "starting point coord ", x, y
+    print "starting radii ", r
+    it = 0
+    gdt = 1
+    l_ori = calculate_segment_lengths(c0,c1,c2,xyz, length_factor)
+    volume = single_bif_volume(r, l_ori)
+    storage = []
+    
+    while it < iter_max and gdt > tolerance:     
+        cvge, x_c, y_c, r_c, l = kamiya_loop_r2(x, y, c0, c1, c2, f, r, length_factor)
+        if (cvge):
+            vol_c = single_bif_volume(r_c,l) 
+            gdt = np.sqrt((x_c - x)**2 + (y_c - y)**2) * length_factor      
+            if (gdt < tolerance) and (vol_c < volume):              
+                print "\n convergence found at iteration ", it
+                #print "position gdt ", gdt
+                #print "volume gdt ", volume - vol_c
+                #print "optimal volume ", vol_c
+                print  "optimal position ", x_c, y_c
+                print "optimal radii ", np.sqrt(r_c)
+                storage.append([x_c, y_c, r_c])
+                return True, storage #x_c, y_c, r
+            else:
+                it = it + 1
+                x , y , volume, r = x_c, y_c, vol_c, r_c
+                if store_whole_results:
+                    storage.append([x, y, r]) #### need it if used in Kamyia testing               
+                #print " position gdt ", gdt
+                #print "volume gdt ", volume - vol_c
+                #print "volume ", vol_c
+                #print "iterating with new position ", it
+        else:
+            print "\n no convergence"
+            return False, storage # x, y, r
+    
+    print " \n max iteration reached"
+    return False, storage # x, y, r
+            
