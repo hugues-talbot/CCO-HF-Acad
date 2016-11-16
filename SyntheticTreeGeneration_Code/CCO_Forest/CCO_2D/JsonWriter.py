7b89277cd3bf806422a25ea02f655b8d0bcecee3
Clara Jaquet <cjaquet@heartflow.com>
2016-11-16 15:21:38 -0800

    creating json writer for 2D Forest

---
+++ SyntheticTreeGeneration_Code/CCO_Forest/CCO_2D/JsonWriter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 09 10:51:12 2016
+
+@author: cjaquet
+"""
+
+#import ForestClass as FC
+import json
+
+def write_json(forest, filename):
+    file_out = open(filename, 'w')
+    fdict={}
+    fdict.update({"Flow" : forest.final_q_perf})
+    fdict.update({"Trees" : len(forest.trees)})
+    fdict.update({"Nterm" : forest.n_term})
+    fdict.update({"FKterm" : forest.get_fk_term()})
+    fdict["Trees"]= {}
+    for tree in forest.trees:
+        fdict["Trees"][tree.tree_index]={}
+        fdict["Trees"][tree.tree_index].update({"Tree flow" : tree.final_q_perf})
+        fdict["Trees"][tree.tree_index].update({"Tree pressure drop" : tree.p_drop})
+        fdict["Trees"][tree.tree_index].update({"Kterm" : tree.get_k_term()})
+        fdict["Trees"][tree.tree_index].update({"Resistance": tree.resistance(0)})
+        fdict["Trees"][tree.tree_index]["Nodes"]= {}
+        for node in tree.nodes:
+            fdict["Trees"][tree.tree_index]["Nodes"][node.index]={}
+            coords = node.coord.tolist()
+            print "coords",coords
+            coords.append(0.)
+            print "coords",coords
+            fdict["Trees"][tree.tree_index]["Nodes"][node.index].update({"Location" : coords})
+            fdict["Trees"][tree.tree_index]["Nodes"][node.index].update({"Parent" : node.parent_index})
+            fdict["Trees"][tree.tree_index]["Nodes"][node.index].update({"Children" : node.children_index.tolist()})
+            fdict["Trees"][tree.tree_index]["Nodes"][node.index].update({"Radius" : tree.get_radius(node.index)})
+            
+    json.dump(fdict, file_out, indent = 4, sort_keys = True)
+    file_out.close()
+            
+    
+    return True
